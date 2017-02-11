e900bcb07d19df217b2438bc51012a311ecf0dce
Winston Venderbush <winstonvenderbush@gmail.com>
2017-02-11 12:24:58 -0500

    fixed source

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from draw import *
 screen = new_screen()
 color = [ 255, 0, 0 ]
 
-draw_line( screen, 15, 15, 150, 150, color )
+draw_line( screen, 0, 0, 500, 500, color )
 
 display(screen)
 save_extension(screen, 'img.png')
