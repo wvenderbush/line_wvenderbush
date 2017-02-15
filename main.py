abbc9f63a029b0da9ffbd040c9ac851bc9d99032
Winston Venderbush <winston@Winstons-MacBook-Pro.local>
2017-02-15 15:37:10 -0500

    finished

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ colorvt = [255, 255, 0]
 color15 = [255, 100, 0]
 color25 = [0, 100, 255]
 
+#TEST CASE
+#---------
+
 draw_line( screen, 150, 150, 400, 350, color1 )
 draw_line( screen, 200, 100, 300, 400, color2 )
 draw_line( screen, 200, 400, 350, 100, color7 ) 
@@ -24,6 +27,27 @@ draw_line( screen, 400, 400, 400, 50, colorvt)
 
 draw_line( screen, 50, 150, 450, 350, color15 )
 draw_line( screen, 50, 350, 450, 200, color25 )
+
+#THE POINT
+#---------
+
+# draw_line( screen, 250, 0, 500, 250, color1)
+# draw_line( screen, 250, 500, 500, 250, color1)
+# draw_line( screen, 0, 250, 250, 0, color1)
+# draw_line( screen, 0, 250, 250, 500, color1)
+
+# draw_line( screen, 0, 0, 500, 500, color2)
+# draw_line( screen, 0, 500, 500, 0, color2)
+
+# draw_line( screen, 250, 0, 250, 500, color7)
+# draw_line( screen, 0, 250, 500, 250, color7)
+
+# draw_line( screen, 125, 0, 375, 500, color8)
+# draw_line( screen, 125, 500, 375, 0, color8)
+
+# draw_line( screen, 0, 375, 500, 125, color8)
+# draw_line( screen, 0, 125, 500, 375, color8)
+
 
 
 
